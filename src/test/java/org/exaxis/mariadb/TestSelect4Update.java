35607c9b586747abe38ae9fa4752c3ff1dd2044f
berryware <david.e.berry@verizon.net>
2025-07-15 08:38:35 -0400

    Simplify tests. Move test data into initDB.sql. Use same container for all tests. Add a non-pooled datasource test

--- src/test/java/org/exaxis/mariadb/TestSelect4Update.java
+++ src/test/java/org/exaxis/mariadb/TestSelect4Update.java
@@ -1,28 +1,46 @@
 package org.exaxis.mariadb;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mariadb.jdbc.MariaDbDataSource;
 import org.mariadb.jdbc.MariaDbPoolDataSource;
+import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.MariaDBContainer;
 import org.testcontainers.utility.DockerImageName;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
-import static org.junit.Assert.fail;
-
 public class TestSelect4Update {
+  private static JdbcDatabaseContainer mariaDB;
+
+  @BeforeAll
+  public static void setup() {
+    mariaDB = new MariaDBContainer(DockerImageName.parse("mariadb:11.8.2"))
+            .withInitScript("initDB.sql")
+            .withDatabaseName("test")
+            .withUsername("test")
+            .withPassword("test");
+    mariaDB.start();
+  }
+
+  @AfterAll
+  public static void teardown() {
+    if (mariaDB!=null)
+      mariaDB.stop();
+  }
 
   @Test
   public void testDriverManager() {
-    var jdbcUrl = "jdbc:tc:mariadb:11.8.2:///test?TC_INITSCRIPT=initDB.sql";
+    var jdbcUrl = mariaDB.withUrlParam("user", mariaDB.getUsername()).withUrlParam("password", mariaDB.getPassword()).getJdbcUrl();
 
     try (Connection conn = DriverManager.getConnection(jdbcUrl)) {
       conn.setAutoCommit(false);
       try (Statement stmt = conn.createStatement()) {
-        stmt.executeUpdate("INSERT INTO test_objects (id, name) values (1, \"record 1\")");
-        stmt.executeUpdate("INSERT INTO test_objects (id, name) values (2, \"record 2\")");
-        conn.commit();
 
         stmt.executeQuery("select * from test_objects where id = 2 for update");
 
@@ -30,75 +48,79 @@ public class TestSelect4Update {
           conn2.setAutoCommit(false);
           try (Statement stmt2 = conn.createStatement()) {
             stmt2.executeQuery("select * from test_objects where id = 2 for update nowait");
-            fail("SelectForUpdateNoWait should fail:");
+            Assertions.fail("SelectForUpdateNoWait should fail:");
           } catch (Exception e) {
             System.err.println(e.getMessage());
           }
         } catch (Exception e) {
           System.err.println(e.getMessage());
-          fail("SelectForUpdateNoWait should get conn2: "+e.getMessage());
+          Assertions.fail("testDriverManager should get conn2: "+e.getMessage());
         }
       } catch (Exception e) {
         System.err.println(e.getMessage());
-        fail("SelectForUpdateNoWait should create stmt:"+e.getMessage());
+        Assertions.fail("testDriverManager should create stmt:"+e.getMessage());
       }
     } catch (Exception e) {
       System.err.println(e.getMessage());
-      fail("SelectForUpdateNoWait should get conn:"+e.getMessage());
+      Assertions.fail("testDriverManager should get conn:"+e.getMessage());
+    }
+  }
+
+  @Test
+  public void testPooledDataSource() {
+    try (var ds = new MariaDbPoolDataSource()) {
+      ds.setUser(mariaDB.getUsername());
+      ds.setPassword(mariaDB.getPassword());
+      ds.setUrl(mariaDB.getJdbcUrl());
+      testSelect4Update(ds);
+    } catch (Exception e) {
+      System.err.println(e.getMessage());
+      Assertions.fail("testPooledDataSource should create datasource:" + e.getMessage());
     }
   }
 
   @Test
   public void testDataSource() {
-    try (var mariaDB = new MariaDBContainer<>(DockerImageName.parse("mariadb:11.8.2"))
-            .withInitScript("initDB.sql")
-            .withDatabaseName("test");
-    ) {
-      mariaDB.start();
-
-      try (var ds = new MariaDbPoolDataSource()) {
-        ds.setUrl(mariaDB.getJdbcUrl());
-        ds.setUser(mariaDB.getUsername());
+      MariaDbDataSource ds;
+      try {
+        ds = new MariaDbDataSource();
         ds.setPassword(mariaDB.getPassword());
+        ds.setUser(mariaDB.getUsername());
+        ds.setUrl(mariaDB.getJdbcUrl());
+        testSelect4Update(ds);
+      } catch (Exception e) {
+        System.err.println(e.getMessage());
+        Assertions.fail("testDataSource should create datasource:" + e.getMessage());
+      }
+  }
 
-        try (Connection conn = ds.getConnection()) {
-          conn.setAutoCommit(false);
-          try (Statement stmt = conn.createStatement()) {
-            stmt.executeUpdate("INSERT INTO test_objects (id, name) values (1, \"record 1\")");
-            stmt.executeUpdate("INSERT INTO test_objects (id, name) values (2, \"record 2\")");
-            conn.commit();
+  private void testSelect4Update(DataSource ds) {
+     try (Connection conn = ds.getConnection()) {
+      conn.setAutoCommit(false);
+      try (Statement stmt = conn.createStatement()) {
 
-            stmt.executeQuery("select * from test_objects where id = 2 for update");
+        stmt.executeQuery("select * from test_objects where id = 2 for update");
 
-            try (Connection conn2 = ds.getConnection()) {
-              conn2.setAutoCommit(false);
-              try (Statement stmt2 = conn.createStatement()) {
-                stmt2.executeQuery("select * from test_objects where id = 2 for update nowait");
-                fail("SelectForUpdateNoWait should fail:");
-              } catch (Exception e) {
-                System.err.println(e.getMessage());
-              }
-            } catch (Exception e) {
-              System.err.println(e.getMessage());
-              fail("SelectForUpdateNoWait should get conn2: "+e.getMessage());
-            }
+        try (Connection conn2 = ds.getConnection()) {
+          conn2.setAutoCommit(false);
+          try (Statement stmt2 = conn.createStatement()) {
+            stmt2.executeQuery("select * from test_objects where id = 2 for update nowait");
+            Assertions.fail("SelectForUpdateNoWait should fail:");
           } catch (Exception e) {
             System.err.println(e.getMessage());
-            fail("SelectForUpdateNoWait should create stmt:"+e.getMessage());
           }
         } catch (Exception e) {
           System.err.println(e.getMessage());
-          fail("SelectForUpdateNoWait should get conn:"+e.getMessage());
+          Assertions.fail("testSelect4Update should get conn2: "+e.getMessage());
         }
       } catch (Exception e) {
         System.err.println(e.getMessage());
-        fail("SelectForUpdateNoWait should create datasource:"+e.getMessage());
+        Assertions.fail("testSelect4Update should create stmt:"+e.getMessage());
       }
     } catch (Exception e) {
       System.err.println(e.getMessage());
-      fail("SelectForUpdateNoWait should create test container:"+e.getMessage());
+      Assertions.fail("testSelect4Update should get conn:"+e.getMessage());
     }
-
   }
 
 }
--- src/test/resources/initDB.sql
+++ src/test/resources/initDB.sql
@@ -4,4 +4,7 @@ name  varchar(255)  NULL
 )
 ENGINE = 'InnoDB';
 
+INSERT INTO test_objects (id, name) values (1, "record 1");
+INSERT INTO test_objects (id, name) values (2, "record 2");
+
 COMMIT;
